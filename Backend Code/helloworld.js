82305a6f1b747a042769f02555134866b83007e6
SamuelJBland <32529391+SamuelJBland@users.noreply.github.com>
2018-10-29 21:33:36 +0000

    Initial front end design
    Initial front end design coding. HTML, CSS formatting etc.

--- Backend Code/helloworld.js
+++
@@ -1,8 +0,0 @@
-var User = /** @class */ (function () {
-    function User(nameRef) {
-        this.name = nameRef;
-    }
-    return User;
-}());
-var user = new User("test");
-console.log(user.name);
---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-2_bwEXCtjeW',
+        userPoolClientId: '700bqtipnvkiqc7vq42f4b33t9',
+        region: 'eu-west-2'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
