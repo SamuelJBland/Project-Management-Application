39fcc83ded20e32857d613abde356b7d72d25823
SamuelJBland <32529391+SamuelJBland@users.noreply.github.com>
2018-11-03 13:13:11 +0000

    CSS Formatting & Font Awesome
    CSS formatting and addition of Font Awesome icons

--- js/react_object_creation.js
+++ js/react_object_creation.js
@@ -59,28 +59,26 @@ function loadProjects() {
       $('#projects').html('');
 
       data.Items.forEach(function(projectItem){
-        //$('#projects').append(
-          //'<div class="card"> <tr> <th>'
-          //  + projectItem.projectID +
-          //'</th> <th>'
-          //  + projectItem.projectName +
-          //'</th> </tr></div>'
-
-
-
           $('#projects').append(
-            '<div class="card">' +
-              '<div class="card-body">' +
-                '<div class="card-title">' +
-                  projectItem.projectName +
+            '<div class="col-md-6" style="margin: 0.5%;">' +
+              '<div class="card">' +
+                '<div class="card-body row">' +
+                  '<div class="col-md-10">' +
+                    '<div class="card-title">' +
+                      projectItem.projectName +
+                    '</div>' +
+                    '<div class="card-text">' +
+                      projectItem.projectName +
+                    '</div>' +
+                  '</div>' +
+                  '<div class="col-md-2">' +
+                    '<button class="btn col-sm-12 row" type="submit" formaction="login();" style="margin: 2%;"> <i class="fas fa-edit"></i></button>' +
+                    '<button class="btn col-sm-12 row" type="submit" formaction="login();" style="margin: 2%;"> <i class="fas fa-trash-alt"></i></button>' +
+                  '</div>' +
                 '</div>' +
-                '<div class="card-text">' +
-                  projectItem.projectName +
-                '</div>' +
-                '<button class="btn" type="submit" formaction="login()"> <i class="fas fa-edit"></i></button>' +
-                '<button class="btn" type="submit" formaction="login()"> <i class="fas fa-trash-alt"></i></button>' +
               '</div>' +
             '</div>'
+
           )
         });
       }
